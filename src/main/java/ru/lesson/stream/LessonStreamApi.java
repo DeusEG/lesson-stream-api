6801698ee32d3ce63e81405664869e6f0b931fcd
Deus <nu_eg@mail.ru>
2023-12-14 19:53:05 +0300

    add 1-3 tasks

--- src/main/java/ru/lesson/stream/LessonStreamApi.java
+++ src/main/java/ru/lesson/stream/LessonStreamApi.java
@@ -3,8 +3,10 @@ package ru.lesson.stream;
 import ru.lesson.stream.dto.Employee;
 import ru.lesson.stream.dto.PositionType;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LessonStreamApi {
 
@@ -14,7 +16,10 @@ public class LessonStreamApi {
      * Важно: Необходимо учесть, что List<Employee> employees может содержать дублирующие записи.
      */
     public List<Employee> task1(List<Employee> employees) {
-        return null;
+        return employees.stream()
+                .filter(e -> e.getRating() > 50)
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     /**
@@ -23,7 +28,11 @@ public class LessonStreamApi {
      * У которых рейтинг {@link Employee#getRating()} меньше 50.
      */
     public List<String> task2(List<Employee> employees) {
-        return null;
+
+        return employees.stream()
+                .filter(e -> e.getRating() < 50)
+                .map(e -> e.getName() + "=" + e.getRating() )
+                .collect(Collectors.toList());
     }
 
     /**
@@ -31,7 +40,10 @@ public class LessonStreamApi {
      * Получить средний рейтнг всех сотрудников.
      */
     public double task3(List<Employee> employees) {
-        return 0.0;
+        return employees.stream()
+                .mapToDouble(Employee::getRating)
+                .average()
+                .orElse(0);
     }
 
     /**
